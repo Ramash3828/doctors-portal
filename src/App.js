aef3878898103ba6b6f98988e2ca508033df2e7a
Ramash3828 <ramash3828@gmail.com>
2022-05-10 15:11:19 +0600

    responsive menubar done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { Route } from "react-router-dom";
+import { Routes } from "react-router-dom";
 import "./App.css";
+import About from "./pages/About/About";
+import Home from "./pages/Home/Home";
+import Navbar from "./Shared/Navbar";
 
 function App() {
     return (
         <div className="App">
-            <h2>Welcome to Doctors Portal</h2>
+            <Navbar></Navbar>
+            <Routes>
+                <Route path="/" element={<Home></Home>}></Route>
+                <Route path="/about" element={<About></About>}></Route>
+                <Route path="/appointment" element={<About></About>}></Route>
+                <Route path="/reviews" element={<About></About>}></Route>
+                <Route path="/contact" element={<About></About>}></Route>
+                <Route path="/login" element={<About></About>}></Route>
+            </Routes>
         </div>
     );
 }
